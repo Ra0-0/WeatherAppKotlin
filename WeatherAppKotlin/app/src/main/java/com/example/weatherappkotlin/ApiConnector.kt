dce889bc94da645c2471242eaa97859bd4e15419
zZzZzZzZz <zzzzz1234555@yandex.ru>
2022-04-22 22:04:07 +0300

    Library Room Added

---
+++ WeatherAppKotlin/app/src/main/java/com/example/weatherappkotlin/ApiConnector.kt
@@ -0,0 +1,50 @@
+package com.example.weatherappkotlin
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+data class WeatherLatLon(
+    var lat: String? = null,
+    var lon: String? = null
+)
+
+data class Main(
+    var temp: String? = null
+)
+data class Wind(
+    var speed: String? = null
+)
+data class CityWeather(
+    var main : Main,
+    var wind : Wind
+)
+
+interface WeatherApiConncetor {
+
+    @GET("geo/1.0/direct")
+    fun directGeocoding(
+        @Query("q") query: String?,
+        @Query("limit") limit: Int?,
+        @Query("appid") appId: String): Call<MutableList<WeatherLatLon>>
+
+    @GET("data/2.5/weather")
+    fun cityWeather(
+        @Query("lat") lat: String?,
+        @Query("lon") lon: String?,
+        @Query("units") units: String,
+        @Query("appid") appId: String): Call<CityWeather>
+
+    companion object ApiAdapter {
+        val baseUrl = "http://api.openweathermap.org/";
+        fun apiConnector(): WeatherApiConncetor {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(baseUrl)
+                .build()
+            return retrofit.create(WeatherApiConncetor::class.java);
+        }
+    }
+}
--- WeatherAppKotlin/app/src/main/java/com/example/weatherappkotlin/MainActivity.kt
+++ WeatherAppKotlin/app/src/main/java/com/example/weatherappkotlin/MainActivity.kt
@@ -1,78 +1,89 @@
 package com.example.weatherappkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
+import androidx.annotation.WorkerThread
+import androidx.appcompat.app.AppCompatActivity
+import androidx.room.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.security.AccessController.getContext
+import java.util.concurrent.Flow
+
 
 class MainActivity : AppCompatActivity() {
-    data class Weather(
-        var lat: String? = null,
-        var lon: String? = null
-    )
 
-    interface WeatherApiService {
+    @Database(entities = [City::class], version = 1, exportSchema = false)
+    abstract class CitiesDatabase : RoomDatabase() {
+        abstract fun citiesDao(): CitiesDao
 
-        @GET("geo/1.0/direct")
-        fun directGeocoding(
-            @Query("q") query: String?,
-            @Query("limit") limit: Int?,
-            @Query("appid") appId: String): Call<MutableList<Weather>>
-
-        @GET("data/2.5/weather")
-        fun cityWeather(
-            @Query("lat") lat: String?,
-            @Query("lon") lon: String?,
-            @Query("appid") appId: String): Call<MutableList<Weather>>
-
-        companion object ApiAdapter {
-            val baseUrl = "http://api.openweathermap.org/";
-            fun apiConnector(): WeatherApiService {
-                val retrofit = Retrofit.Builder()
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl(baseUrl)
-                    .build()
-                return retrofit.create(WeatherApiService::class.java);
+        companion object {
+            @Volatile
+            private var INSTANCE: CitiesDatabase? = null
+            fun getDatabase(context: Context): CitiesDatabase {
+                return INSTANCE ?: synchronized(this) {
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        CitiesDatabase::class.java,
+                        "citiesWeather_database"
+                    ).build()
+                    INSTANCE = instance
+                    // return instance
+                    instance
+                }
             }
         }
     }
+
+    @Entity(tableName = "cities_table")
+    class City (
+        @PrimaryKey(autoGenerate = true)
+        var id : Int? = null,
+        @ColumnInfo(name = "name")
+        var name: String? = null
+    )
+
+    @Dao
+    interface CitiesDao {
+        @Insert(onConflict = OnConflictStrategy.IGNORE)
+        fun insert(сity: City)
+
+        @Query("SELECT * FROM cities_table")
+        fun getAllCities(): List<City>
+    }
+
+    private var db: CitiesDatabase? = null
+    private var context = this
+    private var citiesDao: CitiesDao? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var citiesLatLonLat : String? = null;
-        var citiesLatLonLon : String? = null;
-        
-        WeatherApiService.apiConnector().directGeocoding("Лондон",
-            1, "360cd16dd45ed62190214332afb02a73").enqueue(object : Callback<MutableList<Weather>> {
-            override fun onFailure(call: Call<MutableList<Weather>>, t: Throwable) {
-                Log.i("ApiConectorCall", t.toString())
-            }
+//        var citiesLat : String? = null
+//        var citiesLon : String? = null
 
-            override fun onResponse(call: Call<MutableList<Weather>>, response: Response<MutableList<Weather>>) {
-                val citiesLatLon = response.body();
-                citiesLatLonLat = citiesLatLon?.first()?.lat!!.toString()
-                citiesLatLonLon = citiesLatLon?.first()?.lat!!.toString()
-                Log.i("ApiConectorCall", citiesLatLonLat.toString())
-                Log.i("ApiConectorCall", citiesLatLonLon.toString())
-            }
-        })
-        WeatherApiService.apiConnector().cityWeather(citiesLatLonLat, citiesLatLonLon,
-            "360cd16dd45ed62190214332afb02a73").enqueue(object : Callback<MutableList<Weather>> {
-            override fun onFailure(call: Call<MutableList<Weather>>, t: Throwable) {
+        GlobalScope.launch {
+//            val citiesLatLon = WeatherApiConncetor.apiConnector().directGeocoding("Набережные Челны",
+//                1, "360cd16dd45ed62190214332afb02a73").execute().body()
+//            citiesLat = citiesLatLon?.first()?.lat.toString()
+//            citiesLon = citiesLatLon?.first()?.lon.toString()
+//            val citiesWeather = WeatherApiConncetor.apiConnector().cityWeather(citiesLat, citiesLon,
+//                "metric", "360cd16dd45ed62190214332afb02a73").execute().body()
+//            Log.i("ApiCon", citiesWeather.toString())
+            db = CitiesDatabase.getDatabase(context = context)
+            citiesDao = db?.citiesDao()
 
+            var city1 = City(name = "s")
+
+            with(citiesDao){
+                this?.insert(city1)
             }
-            override fun onResponse(call: Call<MutableList<Weather>>,
-                                    response: Response<MutableList<Weather>>) {
-                Log.i("ApiConectorCall", response.body().toString())
-            }})
+            var list = db?.citiesDao()?.getAllCities()
+            Log.i("ListApp", list.toString())
+            Log.i("ListApp", list?.get(1)?.name.toString())
+        }
 
     }
 
